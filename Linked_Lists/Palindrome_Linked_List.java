3f3d5a2db6bea8a77ae8e714f701c24455569078
Prince-Bhandari <princebhandari67@gmail.com>
2026-06-08 21:17:18 +0530

    Day 5

--- Linked_Lists/Palindrome_Linked_List.java
+++ Linked_Lists/Palindrome_Linked_List.java
@@ -8,6 +8,27 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+// Array Approach - O(n) time, O(n) space
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        List<Integer> list = new ArrayList();
+        while(head !=null){
+            list.add(head.val);
+            head=head.next;
+        }
+        int left = 0;
+        int right = list.size()-1;
+        while(left < right && list.get(left) == list.get(right)){
+            left++;
+            right--;
+        }
+        return left >= right;
+    }
+}
+
+
+// Stack Approach - O(n) time, O(n) space
 class Solution {
     public boolean isPalindrome(ListNode head) {
         Stack<Integer> stack = new Stack<>();
@@ -21,5 +42,57 @@ class Solution {
             curr=curr.next;
         }
         return curr == null;
+    }
+}
+
+
+// Recursion Approach - O(n) time, O(n) space
+class Solution {
+    ListNode curr;
+    public boolean isPalindrome(ListNode head) {
+        curr = head;
+        return solve(head);
+    }
+
+    public boolean solve(ListNode head) {
+        if(head == null) return true;
+        boolean ans = solve(head.next) && head.val == curr.val;
+        curr = curr.next;
+        return ans;
+    }
+}
+
+
+// Reversing Half and Comparing Approach - O(n) time, O(1) space
+class Solution {
+
+    public ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while(curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode rev = reverse(slow.next);
+        slow.next = null;
+        while(rev != null) {
+            if(head.val != rev.val) {
+                return false;
+            }
+            head = head.next;
+            rev = rev.next;
+        }
+        return true;
     }
 }
